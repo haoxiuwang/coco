1835140a0a7caa4b882cda6778dfd2f12f9acc54
whxcctv <whxcctv@gmail.com>
2025-08-20 23:00:25 -0700

    update

---
+++ coco/cocoApp.swift
@@ -0,0 +1,17 @@
+//
+//  cocoApp.swift
+//  coco
+//
+//  Created by Xuanyu on 2025-08-20.
+//
+
+import SwiftUI
+
+@main
+struct cocoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
